e1f76e1a2d1d7d460e0e20812d15be35dbc56f57
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-12 21:38:32 +0100

    creating database

---
+++ app/src/main/java/coj/example/pwdevapp/db.kt
@@ -0,0 +1,7 @@
+package coj.example.pwdevapp
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteException
