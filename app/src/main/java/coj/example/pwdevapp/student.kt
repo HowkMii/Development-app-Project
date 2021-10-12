16785448d4554b728522bd8ec0feee2a2eb9b28e
howkmii <abdelhakim@univ-tiaret.dz>
2021-10-12 22:10:26 +0100

    std class

---
+++ app/src/main/java/coj/example/pwdevapp/student.kt
@@ -0,0 +1,13 @@
+package coj.example.pwdevapp
+
+class student {
+    var stdID:Int?=null
+    var Fname:String?=null
+    var Lname:String?=null
+    constructor(stdID:Int,Fname:String,Lname:String){
+        this.stdID=stdID
+        this.Fname=Fname
+        this.Lname=Lname
+
+    }
+}
